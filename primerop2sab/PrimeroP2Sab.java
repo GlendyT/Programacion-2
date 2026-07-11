8633f2181de264ecd54944e83c3ea9af9733ca72
GlendyT <glendytuyuc@gmail.com>
2026-07-11 15:57:42 -0600

    java

---
+++ primerop2sab/PrimeroP2Sab.java
@@ -0,0 +1,20 @@
+package primerop2sab;
+
+public class PrimeroP2Sab
+{
+
+	/*
+	1 int
+	2 string
+	
+	 la diferencia entre variables y apuntadores repaso
+	*/
+
+	/**
+	 * 
+	 * 
+	 */
+
+
+}
+
--- recursividad.cpp
+++ recursividad.cpp
@@ -1,8 +1,8 @@
 // recursividad llamarse a el mismo
 
-
-int recurs(int n){
-    if(n<5)
-    recurs(r + 1)
-return 0;
+int recurs(int r)
+{
+    if (n < 5)
+        recurs(r + 1);
+    return 0;
 }
